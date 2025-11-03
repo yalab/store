ab4bfca2b5e759f05cd5ad75331419560fe67b55
yalab <rudeboyjet@gmail.com>
2025-11-03 16:09:05 +0900

    Remove unnecessary comments and clean up code structure

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,25 +1,20 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[ show edit update destroy ]
 
-  # GET /products or /products.json
   def index
     @products = Product.all
   end
 
-  # GET /products/1 or /products/1.json
   def show
   end
 
-  # GET /products/new
   def new
     @product = Product.new
   end
 
-  # GET /products/1/edit
   def edit
   end
 
-  # POST /products or /products.json
   def create
     @product = Product.new(product_params)
 
@@ -34,7 +29,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /products/1 or /products/1.json
   def update
     respond_to do |format|
       if @product.update(product_params)
@@ -47,7 +41,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # DELETE /products/1 or /products/1.json
   def destroy
     @product.destroy!
 
@@ -58,12 +51,10 @@ class ProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def product_params
       params.expect(product: [ :name ])
     end
